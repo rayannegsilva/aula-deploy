0ae2cbbf0d84a8d725cf47f43ca4713b4177e55c
rayannegsilva <rgilodasilva@gmail.com>
2025-12-04 23:12:54 -0300

    remove env

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { useState } from "react";
 
 export default function Home() {
-  const URL_POKEMON = process.env.NEXT_PUBLIC_API_URL
+  const URL_POKEMON = "https://pokeapi.co/api/v2/pokemon"
 
   const [dado, setDado] = useState(null);
   const [nome, setNome] = useState();
