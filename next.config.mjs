49f91abddd43a49d40df6f0371b535b029a03c86
rayannegsilva <rgilodasilva@gmail.com>
2025-12-04 22:36:52 -0300

    add env file

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,20 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.petz.com.br",
+        pathname: "/blog/wp-content/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        pathname: "/PokeAPI/sprites/master/sprites/pokemon/**",
+      },
+    ]
+  }
 };
 
 export default nextConfig;
---
+++ src/app/page.jsx
@@ -0,0 +1,56 @@
+'use client';
+
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Home() {
+  const URL_POKEMON = process.env.NEXT_PUBLIC_API_URL;
+
+  const [dado, setDado] = useState(null);
+  const [nome, setNome] = useState();
+
+  async function fetchPokemon(nome) {
+    if (!nome) return;
+
+    try {
+      const response = await fetch(`${URL_POKEMON}/${nome}`)
+      const data = await response.json();
+      setDado(data);
+    } catch (error) {
+      console.error("Erro ao buscar Pokémon:", error);
+    }
+  }
+
+  function handleSetNome(event) {
+    setNome(event.target.value)
+  }
+
+  function onSubmit(event) {
+    event.preventDefault()
+    fetchPokemon(nome)
+  }
+
+
+  return (
+    <div>
+      <h1 className="text-3xl">Página inicial "/"</h1>
+
+      <form onSubmit={onSubmit}>
+        <input type="text" onChange={handleSetNome} />
+        <button type="submit">Pesquisar pokemon</button>
+      </form>
+      {dado && (
+        <div>
+          <p>{dado.name}</p>
+          <Image src={dado.sprites.front_default} height={200} width={200}
+          alt={"carrega"}
+          />
+        </div>
+
+      )}
+    </div>
+  );
+}
+
+
+
